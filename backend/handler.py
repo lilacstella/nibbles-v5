465c8d76727f0e7bb4bd8c753a670bf8d0e80bf8
Emmie Teng <mteng@tamu.edu>
2025-10-04 20:08:06 -0700

    connection to db

--- backend/handler.py
+++ backend/handler.py
@@ -1,13 +1,22 @@
 import tomllib
 from sqlalchemy import create_engine
 
-with open('config/env.toml', 'rb') as f:
-    config = tomllib.load(f)
-    
-connection_string = config['dev']['connection_string']
-engine = create_engine(connection_string)
+engine = None
 
-# check connection
-with engine.connect() as conn:
-    print("Database connection successful")
-    conn.close()
+def setup_database():
+    global engine
+    
+    with open('config/env.toml', 'rb') as f:
+        config = tomllib.load(f)
+        
+    connection_string = config['dev']['connection_string']
+    engine = create_engine(connection_string)
+
+    # check connection
+    try:
+        with engine.connect() as conn:
+            print("Database connection successful")
+            conn.close()
+
+    except Exception as e:
+        print(f"Database connection failed: {e}")
