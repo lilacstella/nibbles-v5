47fda83499be859ea0d29e026f4eb24f5bc8c035
Stella Yang <stella@stellayang.dev>
2025-10-18 18:03:34 -0700

    move assignments out to a function that does one job

--- core/secret_santa_service.py
+++ core/secret_santa_service.py
@@ -5,6 +5,21 @@ from models.secret_santa_model import SecretSantaAssignment, SecretSantaContext
 import random
 from typing import List
 
+def create_valid_assignment(n: int, gift_count: int, users: List["User"]) -> dict["User", set["User"]]:
+    if n <= gift_count + 1:
+        raise ValueError('Impossible to create secret santa with such arrangement')
+    if gift_count > 5:
+        raise ValueError('Please request explicit privileges to have more gifting assignment')
+
+    unique_random_offsets = []
+    while len(set(unique_random_offsets)) < gift_count:
+        unique_random_offsets = [random.randint(1, n - 1) for _ in range(gift_count)]
+
+    assignments = {}
+    for i in range(n):
+        assignments[users[i]] = set(users[(i + offset) % n] for offset in unique_random_offsets)
+    return assignments
+
 def create_secret_santa_game(guild_id: int | None, channel_id: int, user_ids: set[int], gift_count: int) -> None:
     """
     Registers users, randomizes assignments, and commits Secret Santa assignments to the DB.
@@ -18,17 +33,7 @@ def create_secret_santa_game(guild_id: int | None, channel_id: int, user_ids: se
         users: List["User"] = [User.get_or_create(session, _id) for _id in user_ids]
     n = len(user_ids)
 
-    if n <= gift_count + 1:
-        raise ValueError('Impossible to create secret santa with such arrangement')
-    if gift_count > 5:
-        raise ValueError('Please request explicit privileges to have more gifting assignment')
-
-    unique_random_offsets = []
-    while len(set(unique_random_offsets)) < gift_count:
-        unique_random_offsets = [random.randint(1, n - 1) for _ in range(gift_count)]
-    assignments = {}
-    for i in range(n):
-       assignments[users[i]] = set(users[(i + offset) % n] for offset in unique_random_offsets)
+    assignments = create_valid_assignment(n, gift_count, users)
 
     with session_maker() as session:
         game = SecretSantaContext(guild_id=guild_id, channel_id=channel_id)
