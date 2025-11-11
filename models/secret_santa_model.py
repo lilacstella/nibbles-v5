64502d33ce89957c9ff3e64265c8bdce67e82f46
Stella Yang <stella@stellayang.dev>
2025-11-10 19:54:45 -0600

    unused import

--- models/secret_santa_model.py
+++ models/secret_santa_model.py
@@ -1,5 +1,5 @@
 from sqlalchemy import ForeignKey
-from sqlalchemy.orm import Mapped, mapped_column, relationship, Session
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 from typing import TYPE_CHECKING, Any
 
 from models.base import Base
